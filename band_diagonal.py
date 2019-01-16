c1a0e890fac8d92d32c31621caffb3ca25aabb99
Jeff M <infinite.jeff@gmail.com>
2019-01-16 08:26:02 -0700

    Update band_diagonal.py

--- band_diagonal.py
+++ band_diagonal.py
@@ -1,1 +1,74 @@
 
+
+def metropolis(e1, e2, t):
+    """
+    Evaluate metropolis condition with:
+    
+      e1 :  Energy function for current state
+      e2 :  Energy function for proposed state
+      t  :  temperature
+      
+    Always swap if new state is better.  Take disadvantageous swap with probability
+    p = exp(-t*(e2-e1)/tau) (probability decreases exponentially with increasing 
+    iterations - "cooling" in simulated annealing)
+    """
+    if e2 < e1:
+        return True
+    
+    x = (e2-e1)/t
+    
+    if np.random.rand() < np.exp(-x):
+        return True
+    return False
+
+
+def simulated_annealing(x0, energy_fun, perm_fun, 
+                        steps=10000, t0=1.0, tau=1,
+                        report_steps=100,
+                        report=False):
+    x = x0.copy()
+    e = np.zeros(shape=(steps,))
+    swaps = list()
+    for t in np.arange(0, steps):
+        x_cand = x.copy()
+        perm_fun(x_cand)
+        e1 = energy_fun(x)
+        e2 = energy_fun(x_cand)
+        swap = metropolis(e1, e2, tau/t)
+        if swap:
+            x = x_cand
+            e[t] = e2
+        else:
+            e[t] = e1
+        swaps.append(swap)
+            
+        if report and t % report_steps == 0:
+            print('[{0}] Energy = {1}'.format(t, e[t]))
+        
+    return x, e, swaps
+
+
+def permute_two(mat, i1, i2):
+    """
+    Re-order matrix by swapping two rows / columns
+    """
+    if i1 == i2:
+        return
+    
+    # Row exchange
+    row1 = np.array(mat[i1, :])
+    row2 = np.array(mat[i2, :])
+    mat[i1, :] = row2
+    mat[i2, :] = row1
+    
+    # Column exchange
+    col1 = np.array(mat[:, i1])
+    col2 = np.array(mat[:, i2])
+    mat[:, i1] = col2
+    mat[:, i2] = col1
+    
+
+def random_permute_two(mat):
+    i1 = np.random.randint(0, mat.shape[0])
+    i2 = np.random.randint(0, mat.shape[0])
+    permute_two(mat, i1, i2)
